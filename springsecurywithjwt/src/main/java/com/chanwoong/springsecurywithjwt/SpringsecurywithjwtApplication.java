a1d007397a021970dd22b0e87ca32341af65aaef
Chanwoong Park <cksdnddlek99@gmail.com>
2024-03-11 18:40:29 +0900

    Add: 프로젝트 생성 및 의존성 추가

---
+++ springsecurywithjwt/src/main/java/com/chanwoong/springsecurywithjwt/SpringsecurywithjwtApplication.java
@@ -0,0 +1,13 @@
+package com.chanwoong.springsecurywithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurywithjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurywithjwtApplication.class, args);
+	}
+
+}
---
+++ springsecurywithjwt/src/test/java/com/chanwoong/springsecurywithjwt/SpringsecurywithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanwoong.springsecurywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurywithjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
